60ce27dd6eb419bd6c5e4f1d57fad390e1f5fb9c
FedoskinNikita <57385848+FedoskinNikita@users.noreply.github.com>
2019-11-05 10:00:53 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+// Задание 1 
+<?php
+echo "<table width= 100px height= 100px border=4>";
+for($i=1;$i<4;$i++)
+{
+echo "<tr>";
+ for($j=1;$j<4;$j++)
+{
+echo "<td>$i-$j</td>";
+}
+echo "</tr>";
+}
+echo "</table>"; 
+?>
+
+
